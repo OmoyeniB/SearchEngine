e38564bf2b0b3b60611909cda71a8dcc4ab006bf
SharonB9 <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>
2022-04-01 03:25:32 +0100

    constants_file_created_to_handle_strings

--- SearchEngine/SearchEngine/View/MainViewController.swift
+++ SearchEngine/SearchEngine/View/MainViewController.swift
@@ -12,12 +12,32 @@ class MainViewController: UIViewController {
     
     override func loadView() {
         super.loadView()
+        setUpWebKit()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureView()
+       
+       
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//
+//    }
+    
+    func setUpWebKit() {
+        
+        let url = Constants.url
+        guard let url = url else {
+            return
+        }
+        webKitView.load(URLRequest(url: url))
+        webKitView.allowsBackForwardNavigationGestures = true
+        webKitView.allowsLinkPreview = true
+        
     }
     
     func configureView() {
@@ -32,8 +52,6 @@ class MainViewController: UIViewController {
         view.addSubview(stackView)
         stackView.axis = .horizontal
         stackView.alignment = .center
-//        stackView.distribution = .equalSpacing
-        stackView.backgroundColor = .red
         
         stackView.snp.makeConstraints({make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(-20)
@@ -50,24 +68,26 @@ class MainViewController: UIViewController {
         backButton.setTitle("Back", for: .normal)
         backButton.setTitleColor(.black, for: .normal)
         backButton.isEnabled = false
-        backButton.backgroundColor = .yellow
         
         backButton.snp.makeConstraints({make in
             make.left.top.equalTo(stackView)
             make.width.equalTo(40)
+            make.height.equalTo(stackView.snp.height)
         })
     }
     
     func configureSearchField() {
         stackView.addSubview(searchField)
         searchField.placeholder = "Type here to search..."
-        searchField.backgroundColor = .lightGray
         searchField.isUserInteractionEnabled = true
+        searchField.layer.borderColor = UIColor.lightGray.cgColor
+        searchField.layer.borderWidth = 0.5
         
         searchField.snp.makeConstraints({make in
             make.left.equalTo(backButton.snp.right).offset(10)
             make.center.equalTo(stackView.snp.center)
             make.top.equalTo(stackView)
+            make.height.equalTo(stackView.snp.height)
             
         })
     }
@@ -77,17 +97,15 @@ class MainViewController: UIViewController {
         nextButton.setTitle("Next", for: .normal)
         nextButton.setTitleColor(.black, for: .normal)
         nextButton.isEnabled = false
-        nextButton.backgroundColor = .yellow
         
         nextButton.snp.makeConstraints({make in
-           
             make.right.top.equalTo(stackView)
+            make.height.equalTo(stackView.snp.height)
         })
     }
     
     func configureWebKitView() {
-        webKitView.backgroundColor = .red
-        
+
         view.addSubview(webKitView)
         webKitView.snp.makeConstraints({ make in
             make.top.equalTo(stackView.snp.bottom).offset(5)
