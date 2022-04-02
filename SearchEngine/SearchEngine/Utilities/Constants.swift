37bc0ed69f29ec02c1b656132cac3feb76640035
SharonB9 <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>
2022-04-02 01:34:51 +0100

    utilities_updated

--- SearchEngine/SearchEngine/Utilities/Constants.swift
+++ SearchEngine/SearchEngine/Utilities/Constants.swift
@@ -3,4 +3,7 @@ import Foundation
 enum Constants {
     static let url = "https://www.google.com/"
     static let searchBarPlaceholder = "Type to search"
+    static let errorDecoding = "Response could not be decoded"
+    static let unkownError =  "Unknown error"
+    static let invalidUrl = "Please provide a valid URL"
 }
